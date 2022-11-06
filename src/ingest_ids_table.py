0c8524983c53fbb2357ef3038aef6e25f016e77f
alxfed <alxfed@gmail.com>
2022-11-06 11:18:33 -0600

    pickling

--- src/ingest_ids_table.py
+++ src/ingest_ids_table.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 """...
 """
+import pickle
 from uuid import uuid4
 
-sid = uuid4()
-print(str(sid))
+
+a = {'246': str(uuid4())}
+
+with open('../data/zid_sid.pickle', 'wb') as file:
+    pickle.dump(a, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('../data/zid_sid.pickle', 'rb') as file:
+    b = pickle.load(file)
+
+print('ok')
